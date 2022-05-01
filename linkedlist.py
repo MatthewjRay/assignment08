c77d820498adac496b68dd89bb77c3a1f62ae11f
MatthewjRay <matthew.ray2@gmail.com>
2022-05-01 15:38:44 -0400

    Updates

--- linkedlist.py
+++ linkedlist.py
@@ -7,58 +7,78 @@ Created on Sun May  1 02:30:48 2022
 """
 
 class LinkedList:
+    def init(self):
+        self.head = None
+        nodes = []
+        for node in nodes:
+            list.append(node)
+   
     def __init__(self):
         self.head = None
-        
-    def __str__(self):
-        return " -> ".join([str(x) for x in self])
+         
     
-    def __iter__(self): #iterate through list 
-        current_node = self.head
-        while current_node:
-            value = current_node.value
-            current_node = current_node.next
-            yield value  
-    
-    def insert(self, where, value):
-        node = Node(value)
-        node.next = where
-        prev = None
-        next = self.head
-        while next != where: 
-            prev = next
-            next = next.next
-            if prev: prev.next = node
-            else: self.head = node
+    def insert(self, value, where = None):
+    #------ Check if value for 'insert' is empty. if yes: create new list. if not: proceed with regular insert -------#
+        if where == None:
+            node  = Node(value)
+            self.head = node
             return self
+        else:
+            node = Node(value)
+            if self.head == where: #do we want to insert before head of list?
+                self.head = node
+                next = self.head
+                next.next = where
+                return self
+            prev = None
+            next = self.head
+            while next != where: 
+                prev = next
+                next = next.next
+                if next == where:
+                    prev.next = node
+                    node.next = where
+            return self
+
+    def remove(self, node):
+        next = self.head
+        prev = None
+        while next:
+            if next.value == node:
+                if not prev:
+                    self.head = self.head.next
+                else:
+                    prev.next = next.next
+                break
+            else:
+                prev = next
+                next = next.next
+                
+            return
         
-    def remove(self,value):
-        node = Node(value)
-        prev = None
-        next = self.head
-        while next != node: 
-            prev = next
-            next = next.next
-            if prev: prev.next = node.next
-            else: self.head = node.next
-            node.next = None
-            return self
+    def printLL(self):
+        current = self.head
+        while(current):
+            print(current.value)
+            current = current.next
         
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
         
-first_node = Node('A')
-second_node = Node('B')
-third_node = Node('C')
-fourth_node = Node('D')
+a = Node('A')
+b = Node('B')
+c = Node('C')
+d = Node('D')
 
 ll = LinkedList()
-ll.head = first_node
-first_node.next = second_node
-second_node.next = third_node
-third_node.next = fourth_node
+ll.head = a
+a.next = b
+b.next = c
+c.next = d
+
+ll.insert('P',b)
 
 
-print(ll)
+ll.printLL()
